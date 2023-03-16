3259ccf151b1b0db4d36b43174ab991b3f51d054
Bianca Colombelli <110998923+bicolombelli@users.noreply.github.com>
2023-03-16 15:08:12 -0300

    Questão da questao38.py

---
+++ atividade_1/questao38.py
@@ -0,0 +1,3 @@
+a = int(input("Insira um valor: "))
+b = int(input("Insira outro valor: "))
+print ("Esse é o primeiro valor que você inseriu:", b, "e este é o segundo:", a, "e a soma deles é: ", a+b)
