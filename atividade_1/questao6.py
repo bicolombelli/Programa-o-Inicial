bb83444a4c678d33c282d514bb74ebe5f3dc576f
Bianca Colombelli <110998923+bicolombelli@users.noreply.github.com>
2023-03-16 14:26:54 -0300

    Criação da questao6.py

---
+++ atividade_1/questao6.py
@@ -0,0 +1,12 @@
+lista = ["Dorflex", " Dramin", "Ibuprofeno",
+"Fluoxetina", "Omeprazol","Paracetamo"]
+print(lista)
+del lista [0]
+print(lista)
+lista [2] = 'Ritalina'
+print(lista)
+lista.insert (0,"Dorflex")
+print(lista)
+lista.append(input("insira o remédio no sistema:"))
+print(lista)
+print("1 -", lista[0], "2 -", lista[1], "3 - ", lista[2],"4 -", lista[3], "5 -", lista[4], "6 -", lista [5], "7-", lista [6])
