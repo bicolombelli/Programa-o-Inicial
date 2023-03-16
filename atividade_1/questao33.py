a1ba8aa0b400bc3d101f56a2afe2585aa6bf41d5
Bianca Colombelli <110998923+bicolombelli@users.noreply.github.com>
2023-03-16 14:59:12 -0300

    Criação da questao33.py

---
+++ atividade_1/questao33.py
@@ -0,0 +1,3 @@
+nota = float(input("Digite um valor para uma nota que esteja entre 0 e 10: "))
+while (nota < 0 or nota > 10):
+    nota = float(input("Nota digitada incorretamente. Digite outro valor para nota que esteja entre 0 e 10: "))
