532e401bf32a68fac513c07bdf626c4b3b1f9134
Bianca Colombelli <110998923+bicolombelli@users.noreply.github.com>
2023-03-16 14:48:42 -0300

    Criação da questao26.py

---
+++ atividade_1/questao26.py
@@ -0,0 +1,9 @@
+maçãs= int(input('Quantas maçãs vai querer? '))
+if(dar==1):
+print('Você tem a porcentagem de 75% de maçãs. ')
+elif(dar==2):
+print('Você tem a porcentagem da 50% de maçãs. ')
+elif(dar==3):
+print('Você tem a porcentagem de 25% de maçãs. ')
+else:
+print('Você não tem nada')
