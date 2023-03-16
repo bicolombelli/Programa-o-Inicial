cd46dd517c90f4570086708d8f11b14f6e7b5578
Bianca Colombelli <110998923+bicolombelli@users.noreply.github.com>
2023-03-16 15:07:35 -0300

    Criação da questao37.py

---
+++ atividade_1/questao37.py
@@ -0,0 +1,16 @@
+litros = float(input("Digite quantos litros você quer abastecer: "))
+combustível = input("Digite A para álcool ou G para gasolina: ")
+preço = 0
+if combustível == "A" or combustível == "a":
+    preço = litros * 1.90
+    if litros <= 20:
+        preço -= 1.90 * litros * 3 / 100
+    else:
+        preço -= 1.90 * litros * 5 / 100
+elif combustível == "G" or combustível == "g":
+    preço = litros * 2.5
+    if litros <= 20:
+        preço -= 2.50 * litros * 4 / 100
+    else:
+        preço -= 2.50 * litros * 6 / 100
+print(f"O preço a pagar é R${preço:.2f}")
