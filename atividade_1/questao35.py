d8ca5552bc040104d9bb6a578eba96a90ba90f8f
Bianca Colombelli <110998923+bicolombelli@users.noreply.github.com>
2023-03-16 15:01:11 -0300

    Criação da questao35.py

---
+++ atividade_1/questao35.py
@@ -0,0 +1,6 @@
+num1 = int(input("Insira a primeira idade de Pedro:"))
+num2 = int(input("Insira a segunda idade de Joana:"))
+if (num1 < num2):
+  print("O", num1, "é menor que", num2)
+else:
+  print("O", num1, "é maior que", num2)
