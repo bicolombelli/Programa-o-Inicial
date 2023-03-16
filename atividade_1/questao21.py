7ddc04669c1ec98cc79a5b5f25e81d13ccb52153
Bianca Colombelli <110998923+bicolombelli@users.noreply.github.com>
2023-03-16 14:45:03 -0300

    Criação da questao21.py

---
+++ atividade_1/questao21.py
@@ -0,0 +1,2 @@
+lista = ["Domingo", " Segunda", "Terça", "Quarta", "Quinta","Sexta", "Sábado"]
+print("1 -", lista[0], "2 -", lista[1], "3 - ", lista[2], "4 -", lista[3], "5 -", lista[4], "6 -",lista [5], "7 -", lista [6])
